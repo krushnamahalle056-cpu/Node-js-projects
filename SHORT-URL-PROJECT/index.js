9b277daa67155086e1277ea1248def8df07769e0
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-22 08:43:57 +0530

    require middleware

--- SHORT-URL-PROJECT/index.js
+++ SHORT-URL-PROJECT/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const { connnectToMongoDB } = require("./connect");
+const {restrictToLoggedIn} = require("./middlewares/auth")
 
 const URL = require("./models/url");
 
