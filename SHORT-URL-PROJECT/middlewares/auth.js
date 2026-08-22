8f49a81206e301fe2ee871d2d738eb3a55c3b9b7
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-22 08:12:14 +0530

    add restrict loggedin fun

--- SHORT-URL-PROJECT/middlewares/auth.js
+++ SHORT-URL-PROJECT/middlewares/auth.js
@@ -1,2 +1,16 @@
 const {getUser} = require("../service/auth")
 
+async function restrictToLoggedIn(req, res, next) {
+    const userUid = req.cookies.uid;
+
+    if (!userUid) {
+        return res.redirect("/login");
+    }
+    const user = getUser(userUid);
+    if (!user) {
+        return res.redirect("/login");
+    }
+    req.user = user;
+    next();
+}
+
