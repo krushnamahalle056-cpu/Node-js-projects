3c11cd122a2e4e194124d0c1f953405756c8d197
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-24 23:08:23 +0530

    add token

--- SHORT-URL-PROJECT/middlewares/auth.js
+++ SHORT-URL-PROJECT/middlewares/auth.js
@@ -1,27 +1,37 @@
 const {getUser} = require("../service/auth")
 
 async function restrictToLoggedInUsersOnly(req, res, next) {
-    const userUid = req.cookies.uid;
+    const userUid = req.headers['Authorization'];
 
     if (!userUid) {
         return res.redirect("/login");
     }
-    const user = getUser(userUid);
+
+    const user = await getUser(userUid);
+
     if (!user) {
         return res.redirect("/login");
     }
+
     req.user = user;
     next();
 }
 
-async function checkAuth(req, res, next){
+
+async function checkAuth(req, res, next) {
     const userUid = req.cookies.uid;
 
-    const user = getUser(userUid);
+    if (!userUid) {
+        req.user = null;
+        return next();
+    }
+
+    const user = await getUser(userUid);
 
     req.user = user;
     next();
 }
+
 module.exports = {
     restrictToLoggedInUsersOnly,
     checkAuth,
