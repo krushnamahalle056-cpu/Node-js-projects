e3165f324961a03fa8431902a620cdeadd257443
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-24 09:55:32 +0530

    use jwt (json web token)

--- SHORT-URL-PROJECT/service/auth.js
+++ SHORT-URL-PROJECT/service/auth.js
@@ -1,11 +1,21 @@
-const sessionIdToUserMap = new Map();
+//  const sessionIdToUserMap = new Map();   
+   // this is for stateful authentication, we store the user object in memory for the session
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user);
+const jwt = require('jsonwebtoken');  
+    // this is for stateless authentication, we store the user object in the JWT token
+
+const secret = "Krushna@123";
+
+// function setUser(id, user){
+//     sessionIdToUserMap.set(id, user);
+// }
+
+function setUser(user){
+    return jwt.sign(user, secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    return jwt.verify(token, secret);
 }
 
 module.exports = {
