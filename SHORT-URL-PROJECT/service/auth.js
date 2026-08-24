019f6430fb38ebe9b1adf6d1667b398a072d9d3a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-24 10:43:52 +0530

    add try catch

--- SHORT-URL-PROJECT/service/auth.js
+++ SHORT-URL-PROJECT/service/auth.js
@@ -11,10 +11,14 @@ const secret = "Krushna@123";
 // }
 
 function setUser(user){
-    return jwt.sign(user, secret);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
 function getUser(token){
+    if(!token) return null;
     return jwt.verify(token, secret);
 }
 
