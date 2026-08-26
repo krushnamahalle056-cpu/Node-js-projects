9f416cc9fb77d2de108aa65d8defb227d146e43b
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-26 15:45:13 +0530

    ADMIN Authorization

--- SHORT-URL-PROJECT/index.js
+++ SHORT-URL-PROJECT/index.js
@@ -26,11 +26,11 @@ connnectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 
-
-app.use("/url", restrictToLoggedInUsersOnly, URLRoutes);
+app.use("/url", restrictTo(["USER","ADMIN"]), URLRoutes);
 app.use("/user", userRoute);
-app.use("/", checkAuth,staticRoute);
+app.use("/",staticRoute);
 
 
 app.get("/url/:shortId", async (req, res) => {
