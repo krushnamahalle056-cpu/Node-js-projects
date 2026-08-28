cc9c3fc4b453d81a2d9b281f5a23837fbac20aa3
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-28 20:51:03 +0530

    add post request

--- FILE_UPLODING_CONCEPT/index.js
+++ FILE_UPLODING_CONCEPT/index.js
@@ -1,16 +1,26 @@
 const path = require("path");
 const express = require("express");
+const multer  = require('multer');
 
 const app = express();
 const PORT = 8002;
 
+const upload = multer({ dest: 'uploads/' });  // jo bhi file upload hogi use uploads destination folder me store karega
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());  //middleware
+app.use(express.urlencoded({ extended: false}));  // middleware
 
 app.get("/" , (req,res)=>{
     return res.render("homepage");
 });
+
+app.post("/upload",upload.single('ProfileImage'), (req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+
+    return res.redirect("/");
+})
 
 app.listen(PORT, () => console.log(`Server Started at PORT : 8002`));
