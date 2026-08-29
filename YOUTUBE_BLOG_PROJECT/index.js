646cb85c868bafa4ccace77c23b550a25ead4df5
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-29 20:50:25 +0530

    all file upload

--- YOUTUBE_BLOG_PROJECT/index.js
+++ YOUTUBE_BLOG_PROJECT/index.js
@@ -1,1 +1,14 @@
-console.log("Hello, welcome to the YouTube Blog Project!");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");    // set view engine
+app.set("views" , path.resolve("./views")); // set views directory
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
