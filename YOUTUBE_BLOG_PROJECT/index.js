5f770a91314c5fd21182c7015fe5e8382a87eb0e
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-31 23:19:44 +0530

    route add

--- YOUTUBE_BLOG_PROJECT/index.js
+++ YOUTUBE_BLOG_PROJECT/index.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 
+const userRoute = require("./routes/user");
+
 const app = express();
 const PORT = 3000;
 
@@ -10,5 +12,7 @@ app.set("views" , path.resolve("./views")); // set views directory
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
