b70df7040243f6a0820b0a808deca0ae15c3237b
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-31 23:24:30 +0530

    add routes of files

---
+++ YOUTUBE_BLOG_PROJECT/routes/user.js
@@ -0,0 +1,23 @@
+const express = requeire("express");
+const User = require("../models/user");
+const router = Router();
+
+router.get("./signin", (req,res)=>{
+    return res.render("signin");
+});
+
+router.get("./signup", (req,res)=>{
+    return res.render("signup");
+});
+
+router.post("/signup", async(req,res)=>{
+    const {fullName, email, password } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+    return res.redirect("/")
+});
+
+module.express = router;
