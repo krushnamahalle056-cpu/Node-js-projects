910dbe3b20abeb7f8b3d019b8ad49c9ef797a992
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-01 08:00:58 +0530

    mongoDB connected

--- YOUTUBE_BLOG_PROJECT/index.js
+++ YOUTUBE_BLOG_PROJECT/index.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 3000;
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/blogify")
+.then(e => console.log("MongoDB connected:"));
 
 app.set("view engine", "ejs");    // set view engine
 app.set("views" , path.resolve("./views")); // set views directory
