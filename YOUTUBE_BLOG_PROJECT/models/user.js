eced43171d16208dcac53c494de0f347abd046f2
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-01 07:17:25 +0530

    add hash password function

--- YOUTUBE_BLOG_PROJECT/models/user.js
+++ YOUTUBE_BLOG_PROJECT/models/user.js
@@ -1,3 +1,4 @@
+const { createHmac, randomBytes} = require("crypto");  // crypto is use for hash password
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
@@ -30,6 +31,22 @@ const userSchema = new Schema({
 },
     {timestamps: true}
 );
+
+userSchema.pre("save", function(next){
+    const user = this;
+    
+    if(!user.isModified("password")) return;
+    
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+ });
 
 const User = model("User", userSchema);
 
