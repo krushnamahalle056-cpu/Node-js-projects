666d385e03bdbcf1e484ce9f920011f51eec37cc
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-01 08:04:26 +0530

    change in routes

--- YOUTUBE_BLOG_PROJECT/routes/user.js
+++ YOUTUBE_BLOG_PROJECT/routes/user.js
@@ -1,23 +1,26 @@
-const express = requeire("express");
+const { Router } = require("express");
 const User = require("../models/user");
+
 const router = Router();
 
-router.get("./signin", (req,res)=>{
+router.get("/signin", (req, res) => {
     return res.render("signin");
 });
 
-router.get("./signup", (req,res)=>{
+router.get("/signup", (req, res) => {
     return res.render("signup");
 });
 
-router.post("/signup", async(req,res)=>{
-    const {fullName, email, password } = req.body;
+router.post("/signup", async (req, res) => {
+    const { fullName, email, password } = req.body;
+
     await User.create({
         fullName,
         email,
         password,
     });
-    return res.redirect("/")
+
+    return res.redirect("/");
 });
 
-module.express = router;
+module.exports = router;
