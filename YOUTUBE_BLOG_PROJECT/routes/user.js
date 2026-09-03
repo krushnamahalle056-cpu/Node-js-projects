74df75a27ff722aa5345fbff65f01abaf1c0c553
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-03 23:22:17 +0530

    try catch add in post route of signin

--- YOUTUBE_BLOG_PROJECT/routes/user.js
+++ YOUTUBE_BLOG_PROJECT/routes/user.js
@@ -12,12 +12,16 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signin", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.matchPassword(email, password);
+    try{
+        const { email, password } = req.body;
+        const token = await User.matchPasswordAndGenerateToken(email, password);
     
-    console.log("User" , user);
-    
-    return res.redirect("/");
+        return res.cookie("token", token).redirect("/");
+    } catch (error) {
+        return res.render("signin", {
+            error: "Incorrect email or password"
+        });
+    }
 });
 
 router.post("/signup", async (req, res) => {
