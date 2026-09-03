8a72be82bebe3051c371efa0bfd16db767a2eabf
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-03 23:19:06 +0530

    add middleware for cookie

---
+++ YOUTUBE_BLOG_PROJECT/middlewares/authentication.js
@@ -0,0 +1,16 @@
+function checkForAuthenticationCookie(cookieName) {
+    return (req, res, next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+        if (!tokenCookieValue) {
+           return next();
+        }
+       
+        try{
+            const userPayload = validateToken(tokenCookieValue);
+            req.user = userPayload;
+        }catch(err){}
+        return next();
+
+    };
+}
+
