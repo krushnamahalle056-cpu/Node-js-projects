243db1e213f36ff5f2c440b1c11b95b66f115807
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-05 16:20:43 +0530

    require blog

--- YOUTUBE_BLOG_PROJECT/index.js
+++ YOUTUBE_BLOG_PROJECT/index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 
 const app = express();
 const PORT = 3000;
@@ -16,6 +17,7 @@ mongoose.connect("mongodb://localhost:27017/blogify")
 app.set("view engine", "ejs");    // set view engine
 app.set("views" , path.resolve("./views")); // set views directory
 
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));  // to parse form data
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
@@ -28,5 +30,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
